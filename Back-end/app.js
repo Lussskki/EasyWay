52b5b125f4413bc81dd37db33cc3712d951e3330
Luka Guledani <lussskki@gmail.com>
2025-05-28 20:09:31 +0400

    initial  commit for Back-end branch

--- Back-end/app.js
+++ Back-end/app.js
@@ -1,8 +1,43 @@
-import express from "express"
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+// Routes
+
+
+// Environment variables
+dotenv.config()
+
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
-app.listen(PORT, ()=>{
-    console.log(`Server running on a port ${PORT}`)
+const corsOptions = {
+    origin: "*", 
+    credentials: true, 
+} 
+
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(cookieParser())
+
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.muavqao.mongodb.net/`
+
+// Routes to access
+
+
+
+// Database connection
+mongoose
+    .connect(url)
+    .then(() => console.log("MongoDB connected"))
+    .catch((error) => {
+        console.log("Error", error.message)
+        process.exit(1)
+    })
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
 })
