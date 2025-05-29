ae9b7575f6ea6b150645c9b53d8cb7435a3c37e9
Luka Guledani <lussskki@gmail.com>
2025-05-29 12:28:49 +0400

    add in route express validator for check req.body

--- Back-end/Routes/register-route.js
+++ Back-end/Routes/register-route.js
@@ -1,9 +1,17 @@
 import express from 'express'
 
+import { body } from 'express-validator'
 import {registerController} from '../Controllers/register-controller.js'
 
 const registerRouter = express.Router()
 
-registerRouter.post('/', registerController)
+registerRouter.post(
+    '/',
+    [
+        body('username').isLength({ min: 10 }).withMessage('Username must be at least 10 characters long'),
+        body('email').isEmail().withMessage('Please enter a valid email address'),
+        body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+    ],
+     registerController)
 
 export default registerRouter
