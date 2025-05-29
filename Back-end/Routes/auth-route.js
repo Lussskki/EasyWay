416d18ea8966dd392c6617e9afa68f308a636893
Luka Guledani <lussskki@gmail.com>
2025-05-29 21:31:42 +0400

    add login router

--- Back-end/Routes/auth-route.js
+++ Back-end/Routes/auth-route.js
@@ -19,10 +19,10 @@ const loginRouter = express.Router()
 
 loginRouter.post(
     '/',
-    // [
-    //     body('email').isEmail().withMessage('Please enter a valid email address'),
-    //     body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
-    // ],
+    [
+        body('email').isEmail().withMessage('Please enter a valid email address'),
+        body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+    ],
      loginUser)
 
 export { registerRouter, loginRouter }     
