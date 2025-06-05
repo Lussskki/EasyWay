228bc40a3014cfc8cad4fb37884e8d2484f732ec
Luka Guledani <lussskki@gmail.com>
2025-06-05 15:58:35 +0400

    add res.cookie({})

--- Back-end/Controllers/auth-controller.js
+++ Back-end/Controllers/auth-controller.js
@@ -41,13 +41,22 @@ export const loginUser = async (req, res) => {
 
         console.log('Access Token:', accessToken)
         console.log('Refresh Token:', refreshToken)
-
+        
+        res.cookie('refreshToken', refreshToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'Strict',
+            path: '/',
+            maxAge: rememberMe ? 7 * 24 * 60 * 60 * 1000 : 24 * 60 * 60 * 1000,
+        })
+        
         res.status(200).json({
             message: 'Login successful',
             user: { username: user.username, email: user.email },
             accessToken,
             refreshToken
         })
+
     } catch (error) {
         console.error('Error during login:', error)
         res.status(500).json({ message: 'Internal server error' })
